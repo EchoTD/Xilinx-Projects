2229744cc1b236aaf6cb1e8bd2c290312e23dff8
Can Gursoy <alaaddin.c.gursoy@gmail.com>
2024-10-29 19:44:24 +0300

    yea

--- firstProject/firstProject.srcs/sources_1/new/hw1.vhd
+++ firstProject/firstProject.srcs/sources_1/new/hw1.vhd
@@ -1,8 +1,8 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: yettu
+-- Engineer: acg
 -- 
--- Create Date: 10/23/2024 11:09:21 AM
+-- Create Date: 10/23/2024 11:06:51 AM
 -- Design Name: 
 -- Module Name: logic_ex - Behavioral
 -- Project Name: 
@@ -23,14 +23,17 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity logic_ex is
-    Port ( SW : in STD_LOGIC;
-           LED : out STD_LOGIC);
+    Port (  SW : in STD_LOGIC_VECTOR(1 downto 0);
+            LED: out STD_LOGIC_VECTOR(3 downto 0));
 end logic_ex;
 
 architecture Behavioral of logic_ex is
 
 begin
-LED <= not SW;
-
+    
+LED(0) <= NOT SW(0);
+LED(1) <= SW(0) AND SW(1);
+LED(2) <= SW(0) OR SW(1);
+LED(3) <= SW(0) XOR SW(1);
 
 end Behavioral;
